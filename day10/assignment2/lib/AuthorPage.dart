2f2c7b921be36d9d97a5b4daad2a5388f5d6e55a
dev857394 <dev857394@gmail.com>
2023-10-05 21:42:30 +0900

    Day10 Assignment2 Complete

--- day10/assignment2/lib/AuthorPage.dart
+++ day10/assignment2/lib/AuthorPage.dart
@@ -32,38 +32,42 @@ class _AuthorPageState extends State<AuthorPage> {
               print("=============== AUTHORS : $authors");
             }
           }
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GridView.builder(
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, mainAxisSpacing: 14),
-                itemCount: authors.length,
-                itemBuilder: (context, index) {
-                  return FadeInDown(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.network(
-                          authors[index]["avatar"]!,
-                          height: 100,
-                          width: 100,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          authors[index]["name"]!,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ],
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3, mainAxisSpacing: 14),
+                  itemCount: authors.length,
+                  itemBuilder: (context, index) {
+                    return FadeInDown(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            authors[index]["avatar"]!,
+                            height: 100,
+                            width: 100,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            authors[index]["name"]!,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           );
         },
       ),
